cb8e79f1970ac75a4ebc7507f5e0b708cb05df78
alin piero <alinpiero@gmail.com>
2022-06-09 01:07:12 -0500

    Sumatoria y conteo de valores con el uso de FOR

---
+++ Arreglo3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Arreglo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Console.WriteLine("Hello World Cod-ex!");
+            int contador = 0;
+            int sumatoria = 0;
+            
+            int[] numeros = new int[10] { 1,2,3,4,5,6,7,8,9,10};
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                contador = contador + 1;
+                Console.WriteLine("Posición: " + i + " = " + numeros[i]);
+                sumatoria = sumatoria + numeros[i];
+
+            }
+            Console.WriteLine("Hay " + contador + " Valores");
+            Console.WriteLine("La suma de los " + contador + " digitos es " + sumatoria);
+        }
+    }
+}
